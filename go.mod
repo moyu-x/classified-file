292c6625323cb8a5d38e432552dd5a70a8986690
moyu-x <idwangmo@protonmail.com>
2025-06-08 18:21:32 +0800

    feat: 调整结构

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module classified-file
+module github.com/moyu-x/classified-file
 
-go 1.14
+go 1.24
+
+require github.com/spf13/cobra v1.9.1
 
 require (
-	github.com/google/uuid v1.1.1
-	github.com/h2non/filetype v1.1.0
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,114 +1,28 @@
+/*
+Copyright © 2025 moyu-x
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
-
-	"github.com/google/uuid"
-)
-
-// PreMap 准备初始化数据
-func PreMap() (map[string]int, map[string]int) {
-
-	videoExt := [...]string{"mp4", "mkv", "mov", "webm", "avi", "wmv", "mpg",
-		"flv", "3gp", "rmvb", "MP4", "MPG", "ass", "srt", "ssa", "MOV", "3GP",
-		"asf", "ASF", "AVI", "m4v", "mpeg", "rm", "ts", "WMV"}
-	imageExt := [...]string{"jpg", "png", "gif", "webp", "jpeg", "JPG", "PNG",
-		"bmp", "GIF", "JPEG"}
-	videoExtMap := make(map[string]int)
-	imageExtMap := make(map[string]int)
-
-	for index, item := range videoExt {
-		videoExtMap[item] = index
-	}
-
-	for index, item := range imageExt {
-		imageExtMap[item] = index
-	}
-
-	return videoExtMap, imageExtMap
-}
+import "github.com/moyu-x/classified-file/cmd"
 
 func main() {
-
-	videoExtMap, imageExtMap := PreMap()
-
-	root := ""
-	desRoot := ""
-
-	var files []string
-	var videoFiles []string
-	var imageFiles []string
-
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if !info.IsDir() {
-
-			extSp := strings.Split(info.Name(), ".")
-			extName := extSp[len(extSp)-1]
-
-			if _, ok := videoExtMap[extName]; ok {
-				videoFiles = append(videoFiles, path)
-			} else if _, ok := imageExtMap[extName]; ok {
-				imageFiles = append(imageFiles, path)
-			} else {
-				files = append(files, path)
-			}
-		}
-		return nil
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	pathMap := make(map[string][]string)
-	pathMap["video"] = videoFiles
-	pathMap["image"] = imageFiles
-	pathMap["other"] = files
-
-	fmt.Println(pathMap)
-
-	for k, v := range pathMap {
-		folderPath := path.Join(desRoot, k)
-
-		if _, err := os.Stat(folderPath); os.IsNotExist(err) {
-			os.Mkdir(folderPath, os.ModePerm)
-		}
-
-		for _, file := range v {
-			MoveFile(file, folderPath)
-		}
-
-	}
-}
-
-// MoveFile 移动文件
-func MoveFile(file string, despath string) {
-	fileExtSp := strings.Split(file, "/")
-	name := fileExtSp[len(fileExtSp)-1]
-
-	filename := FilenameCheck(name, despath)
-	err := os.Rename(file, path.Join(despath, filename))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// FilenameCheck 检查并重置路径名称
-func FilenameCheck(filename string, desRoot string) string {
-	if _, err := os.Stat(path.Join(desRoot, filename)); os.IsNotExist(err) {
-		return filename
-	}
-	newName := uuid.New().String() + filename
-	return FilenameCheck(newName, desRoot)
+	cmd.Execute()
 }
